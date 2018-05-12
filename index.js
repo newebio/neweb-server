d33cd99bca6e4523a944a30e105c7afc9f034f9d
Vitaly Arkhipov <arvitaly@list.ru>
2018-05-12 20:15:11 +0300

    added exports

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 "use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
 Object.defineProperty(exports, "__esModule", { value: true });
 var Application_1 = require("./lib/Application");
 exports.Application = Application_1.default;
@@ -10,3 +13,4 @@ var SeancesManager_1 = require("./lib/SeancesManager");
 exports.SeancesManager = SeancesManager_1.default;
 var ClassicRouter_1 = require("./lib/ClassicRouter");
 exports.ClassicRouter = ClassicRouter_1.default;
+__export(require("./lib/ClassicRouter"));
--- index.ts
+++ index.ts
@@ -3,4 +3,5 @@ export { default as FrameController } from "./lib/FrameController";
 export { default as Server } from "./lib/Server";
 export { default as SeancesManager } from "./lib/SeancesManager";
 export { default as ClassicRouter } from "./lib/ClassicRouter";
+export * from "./lib/ClassicRouter";
 export * from "./typings";
